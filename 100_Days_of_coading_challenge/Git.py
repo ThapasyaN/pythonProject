3b160edd4743cfa29bc0cd15c023a3d2f9ae6039
Thapasya <nadigottithapasya@gmail.com>
2024-06-06 10:36:01 +0530

    update

--- 100_Days_of_coading_challenge/Git.py
+++ 100_Days_of_coading_challenge/Git.py
@@ -1,48 +1,56 @@
-# git config --global user.name "ThapasyaN"
-# git config --global user.email <nadigottiThapasya@gmail.com>
-# git init
-# git add *
-# git commit -m "name"
-# git remote add origin https://github.com/ThapasyaN/pythonProject.git
-# git push origin master
+'''git config --global user.name "ThapasyaN"
+git config --global user.email <nadigottiThapasya@gmail.com>
+git init
+git add *
+git commit -m "name"
+git remote add origin https://github.com/ThapasyaN/pythonProject.git
+git push origin master
 
 
-# STATUS: to know the no. of files updated by others to your program.
-# git status
+STATUS: to know the no. of files updated by others to your program.
+git status
 
 
-# STAGES: to send files into stages we need to -
-# git add *          - to add all the files into stages.
-# git add file_name     - to add a particular file into to stages.
+STAGES: to send files into stages we need to -
+git add *          - to add all the files into stages.
+git add file_name     - to add a particular file into to stages.
 
 
-# COMMIT: to send files into the commit we need to -
-# git commit -m'ENTER THE FILE NAME WHICH YOU WANT TO ADDRESS WITH'
+COMMIT: to send files into the commit we need to -
+git commit -m'ENTER THE FILE NAME WHICH YOU WANT TO ADDRESS WITH'
 
 
-# to push the code we need to -
-# git push origen master
+to push the code we need to -
+git push origen master
 
-# https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
-# a link to BASIC GIT COMMANDS.
-
-
-# to check the no. of branches -
-# git branches
+https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
+a link to BASIC GIT COMMANDS.
 
 
-# to create a new branch -
-# git checkout -b name_of_the_branch
+to get entire repository for the first time-
+git clone
 
 
-# to switch the branches to an existing branch -
-# git checkout name_of_the_branch
+to pull the latest code into your machine
+git pull origin master
 
 
-# to merge to branches i.e the merge a branch into our active branch -
-# git merge <name_of_the_branch_you_want_to_merge>
+to check the no. of branches -
+git branches
 
-# Windows powershell
+
+to create a new branch -
+git checkout -b name_of_the_branch
+
+
+to switch the branches to an existing branch -
+git checkout name_of_the_branch
+
+
+to merge to branches i.e the merge a branch into our active branch -
+git merge <name_of_the_branch_you_want_to_merge>
+
+Windows powershell'''
 
 
 
